706fd3eee9b8f0415213c00dbad17116d54b219a
charlieroth <charlieroth4@icloud.com>
2023-07-20 21:26:57 +0200

    Add image uploading for a product

--- lib/pento_web/live/product_live/form_component.ex
+++ lib/pento_web/live/product_live/form_component.ex
@@ -7,10 +7,19 @@ defmodule PentoWeb.ProductLive.FormComponent do
   def update(%{product: product} = assigns, socket) do
     changeset = Catalog.change_product(product)
 
-    {:ok,
-     socket
-     |> assign(assigns)
-     |> assign_form(changeset)}
+    {
+      :ok,
+      socket
+      |> assign(assigns)
+      |> assign_form(changeset)
+      |> allow_upload(
+        :image,
+        accept: ~w(.jpg .jpeg .png),
+        max_entries: 1,
+        max_file_size: 9_000_000,
+        auto_upload: true
+      )
+    }
   end
 
   @impl true
@@ -27,7 +36,13 @@ defmodule PentoWeb.ProductLive.FormComponent do
     save_product(socket, socket.assigns.action, product_params)
   end
 
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :image, ref)}
+  end
+
   defp save_product(socket, :edit, product_params) do
+    product_params = params_with_image(socket, product_params)
+
     case Catalog.update_product(socket.assigns.product, product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -43,6 +58,8 @@ defmodule PentoWeb.ProductLive.FormComponent do
   end
 
   defp save_product(socket, :new, product_params) do
+    product_params = params_with_image(socket, product_params)
+
     case Catalog.create_product(product_params) do
       {:ok, product} ->
         notify_parent({:saved, product})
@@ -57,11 +74,29 @@ defmodule PentoWeb.ProductLive.FormComponent do
     end
   end
 
+  defp params_with_image(socket, params) do
+    path =
+      socket
+      |> consume_uploaded_entries(:image, fn %{path: path}, _entry ->
+        # TODO: Plug in production image file persistence here
+        dest = Path.join([:code.priv_dir(:pento), "static", "uploads", Path.basename(path)])
+        File.cp!(path, dest)
+        {:ok, ~p"/uploads/#{Path.basename(dest)}"}
+      end)
+      |> List.first()
+
+    Map.put(params, "image_upload", path)
+  end
+
   defp assign_form(socket, %Ecto.Changeset{} = changeset) do
     assign(socket, :form, to_form(changeset))
   end
 
   defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+
+  def error_to_string(:too_large), do: "Too large"
+  def error_to_string(:not_accepted), do: "You have selected an unacceptable file type"
+  def error_to_string(:too_many_files), do: "You have selected too many files"
 
   @impl true
   def render(assigns) do
@@ -75,6 +110,7 @@ defmodule PentoWeb.ProductLive.FormComponent do
       <.simple_form
         for={@form}
         id="product-form"
+        multipart={true}
         phx-target={@myself}
         phx-change="validate"
         phx-submit="save"
@@ -83,6 +119,42 @@ defmodule PentoWeb.ProductLive.FormComponent do
         <.input field={@form[:description]} type="text" label="Description" />
         <.input field={@form[:unit_price]} type="number" label="Unit price" step="any" />
         <.input field={@form[:sku]} type="number" label="Sku" />
+
+        <form id="upload-form" phx-submit="save" phx-change="validate">
+          <.live_file_input upload={@uploads.image} />
+          <%!-- <button type="submit">Upload</button> --%>
+        </form>
+
+        <section phx-drop-target={@uploads.image.ref}>
+          <%= for entry <- @uploads.image.entries do %>
+            <article class="upload-entry">
+              <figure>
+                <.live_img_preview entry={entry} />
+                <figcaption><%= entry.client_name %></figcaption>
+              </figure>
+
+              <progress value={entry.progress} max="100" />
+
+              <button
+                type="button"
+                phx-click="cancel-upload"
+                phx-value-ref={entry.ref}
+                aria-label="cancel"
+              >
+                &times;
+              </button>
+
+              <%= for err <- upload_errors(@uploads.image, entry) do %>
+                <p class="alert alert-danger"><%= err %></p>
+              <% end %>
+            </article>
+          <% end %>
+
+          <%= for err <- upload_errors(@uploads.image) do %>
+            <p class="alert alert-danger"><%= error_to_string(err) %></p>
+          <% end %>
+        </section>
+
         <:actions>
           <.button phx-disable-with="Saving...">Save Product</.button>
         </:actions>
